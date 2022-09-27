4b869549e337e4f92752cdae6839161edb7de722
panaselena2130 <104391336+panaselena2130@users.noreply.github.com>
2022-09-28 00:02:28 +0300

    Update test_homepage.py

--- pom/tests/test_homepage.py
+++ pom/tests/test_homepage.py
@@ -5,16 +5,11 @@ def test_login(driver):
     print('___________')
     homepage = HomePage(driver)
     homepage.load_page()
-    # actual_links = homepage.text_of_list_Ind_Homepage()
-    # expected_links = homepage.LINK_TEXT
-    # assert expected_links==actual_links
+    
 
     print(homepage.text_of_list_Ind_Homepage())
     homepage.major_data()
     print(homepage.text_of_list_relat_company())
     homepage.output_text_file()
 
-    # homepage.req_index(driver)
-
-    # homepage.input()
-
+   
