1775d207eb2792310f33e13c16c3f27e0283e61c
panaselena2130 <104391336+panaselena2130@users.noreply.github.com>
2022-10-05 23:02:39 +0300

    Update test_homepage.py

--- pom/tests/test_homepage.py
+++ pom/tests/test_homepage.py
@@ -1,3 +1,4 @@
+import time
 
 from pom.pages.home_page import HomePage
 
@@ -5,16 +6,15 @@ def test_login(driver):
     print('___________')
     homepage = HomePage(driver)
     homepage.load_page()
-
-
-    
-
-    print(homepage.text_of_list_Ind_Homepage())
-
-#     assert homepage.expected_list == homepage.text_of_list_Ind_Homepage()
-
     homepage.major_data()
-    print(homepage.text_of_list_relat_company())
-    homepage.output_text_file()
+    print(homepage.list_of_Ind_Homepage(),'1')
+    print(homepage.text_of_list_Ind_Homepage(),'2')
+
+
+    y=homepage.get_nav_link_by_name('POALIM\nPOLI IL0006625771 3,037 2.5% 149,628.84 EoD 2,963 2,999\nלינקים לפעולות שונות')
+    y.click()
+
+
+
 
    
